78c03adf9714f21de522d8755dff7eb4d9360b3c
Ahmed-py <eng_ahmed_hamed2016@yahoo.com>
2023-04-03 18:22:51 -0700

    function that prints the sum of the two diagonals of a square matrix of integers.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums/ - print/s the/ sum/ of/ the/ two/
+ * print_diagsums - print/s the/ sum/ of/ the/ two/
  * diagonals/ of/ a square/ matrix/ of/ integers/
- * @a/: input pointer/.
- * @size/: size/ of/ the/ matrix/
- * Return/: no return/.
+ * @a: input pointer/.
+ * @size: size/ of/ the/ matrix/
+ * Return: no return/.
  */
 void print_diagsums(int *a, int size)
 {
