50e49bad6b9406010904cd74c0c9c548d172ef96
Ahmed-py <eng_ahmed_hamed2016@yahoo.com>
2023-04-17 17:04:22 -0700

    Define a new type struct dog with the following elements:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,7 @@
 /**
  * dog_t - Typedef for struct dog
  */
-typedef struct dog dog_t;
+typedef struct dog my_dog;
 
 /**
  * struct dog - struct that stores some information of a dog
@@ -24,7 +24,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
 
 #endif
