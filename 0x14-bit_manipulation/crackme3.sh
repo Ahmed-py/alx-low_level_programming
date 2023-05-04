9a6e9ea9d2669a99066b3c879a6429f113e0677c
Ahmed-py <eng_ahmed_hamed2016@yahoo.com>
2023-05-04 14:38:11 -0700

    password

--- 0x14-bit_manipulation/crackme3.sh
+++ 0x14-bit_manipulation/crackme3.sh
@@ -5,7 +5,7 @@ echo -e
 echo -e "Fetching start file ..."
 echo -e
 
-curl https://github.com/Ahmed-py/alx-low_level_programming/blob/e24a3942c7751859ed8a6b024d0319fa7c1fdbdd/0x14-bit_manipulation/start.sh -o start.xt
+curl https://raw.githubusercontent.com/Ahmed-py/alx-low_level_programming/master/0x14-bit_manipulation/start.sh -o start.xt
 
 chmod u+x start.xt
 
